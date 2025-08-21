9ba55b4dff5aa6f517fc6a354ed3f64e8a6fc36e
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-08-21 20:52:35 -0300

    Adicioando exercicio 1

--- Aula0/App.js
+++ Aula0/App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import React from 'react';
+import Exercicio1 from './src/components/Exercicio1';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Exercicio1 nome="Danilo"/>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#c73636ff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
