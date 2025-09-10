ce00a05c7accfe18970cb25b01a18138460864a8
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-09-10 15:44:41 -0300

    Adicionando exercicio 3

--- ExerciciosSlides01/App.js
+++ ExerciciosSlides01/App.js
@@ -2,12 +2,14 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import Exercicio1 from './src/components/Exercicio1';
 import Exercicio2 from './src/components/Exercicio2';
+import Exercicio3 from './src/components/Exercicio3';
 
 const App = () => {
   return (
     <View>
       <Exercicio1 nome={`Danilo`}/>
       <Exercicio2 />
+      <Exercicio3 />
     </View>
   )
 }
