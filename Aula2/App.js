1877737014fe59eb5962f4321775f3199d10932e
d4nkali <133923419+d4nkali@users.noreply.github.com>
2025-09-18 16:26:10 -0300

    Adicionando cartao aula 2

--- Aula2/App.js
+++ Aula2/App.js
@@ -1,15 +1,14 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import LayoutResponsivo from './src/components/LayoutResponsivo';
+import { View, Text } from "react-native";
+import React from "react";
+import CartaoApresentacao from "./src/components/CartaoApresentacao";
 
 const App = () => {
-
     return (
-
-        <View style={{ flex: 1}}>
-            <LayoutResponsivo />
+        <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+            <CartaoApresentacao />
         </View>
-
     );
 };
 
